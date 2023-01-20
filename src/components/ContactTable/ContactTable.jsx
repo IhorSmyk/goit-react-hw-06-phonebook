8a189bf364aa8a0f42c434b82e94e4aa79313f6c
Ihor Smyk <ismyk256@gmail.com>
2023-01-20 13:04:07 +0200

    upd

---
+++ src/components/ContactTable/ContactTable.jsx
@@ -0,0 +1,50 @@
+import PropTypes from 'prop-types';
+import s from './ContactTable.module.css';
+import { useSelector } from 'react-redux';
+
+const ContactTable = ({ onDeleteContact }) => {
+  const contacts = useSelector(state => state.contacts.items);
+  return (
+    <>
+      {contacts.length === 0 ? (
+        <p className={s.message}>There is no contact</p>
+      ) : (
+        <table className={s.contactListTable}>
+          <thead className={s.thead}>
+            <tr>
+              <th>Name</th>
+              <th>Number</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody className={s.tbody}>
+            {contacts.map(({ id, name, number }) => {
+              return (
+                <tr key={id}>
+                  <td>{name} </td>
+                  <td>{number}</td>
+                  <td>
+                    <button onClick={() => onDeleteContact(id)}>delete</button>
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      )}
+    </>
+  );
+};
+
+ContactTable.propTypes = {
+  contacts: PropTypes.arrayOf(
+    PropTypes.exact({
+      id: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      number: PropTypes.string.isRequired,
+    }).isRequired
+  ).isRequired,
+  onDeleteContact: PropTypes.func.isRequired,
+};
+
+export default ContactTable;
---
+++ src/redux/contacts/contacts.slice.js
@@ -0,0 +1,36 @@
+import { createSlice, nanoid } from '@reduxjs/toolkit';
+
+const contactSlice = createSlice({
+  name: 'contacts',
+  initialState: { items: [] },
+  reducers: {
+    addContact: {
+      reducer(state, action) {
+        state.items.unshift(action.payload);
+      },
+      prepare(newContact) {
+        return { payload: { ...newContact, id: nanoid() } };
+      },
+    },
+  },
+});
+
+export const { addContact } = contactSlice.actions;
+
+export default contactSlice.reducer;
+
+// reducers: {
+//     addTask: {
+//       reducer(state, action) {
+//         state.push(action.payload);
+//       },
+//       prepare(text) {
+//         return {
+//           payload: {
+//             text,
+//             id: nanoid(),
+//             completed: false,
+//           },
+//         };
+//       },
+//     },
